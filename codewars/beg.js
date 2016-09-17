937906181b6fcc8530a75432fd455b5d7bc60315
Nohema Call <you@example.com>
2016-09-17 17:20:45 -0600

    newest

--- codewars/beg.js
+++ codewars/beg.js
@@ -85,3 +85,19 @@ function squareDigits(num) {
         return Number(num3);
 
 }
+
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+var capitals = function (word) {
+	var array = [];
+	word = word.split('')
+
+  for (var i = 0; i < word.length; i++) {
+        if(word[i] === word[i].toUpperCase()) {
+          array.push(i);
+
+        }
+  }
+  return array;
+
+}
